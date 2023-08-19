0dcfa284ba119e4d1aaaae5177b84e6f1e012c79
ridsonap <alfrzlp@gmail.com>
2023-08-19 11:32:30 +0700

    first commit

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' mys: prediksi harga xxx.
+#'
+#' A dataset containing the Cost and other attributes of almost 57.971 observation 36 variabels.
+#'
+#' @format A data frame with 57.971 rows and 36 variables:
+#' \describe{
+#'   \item{area}{xxx cost}
+#'   \item{y}{variabel x1}
+#'   \item{var}{variabel x1}
+#'   \item{rse}{variabel x2}
+#'   \item{x1}{date}
+#'   \item{x2}{baris id}
+#'   \item{x3}{baris id}
+#' }
+#' @source \url{https://apaanini}
+"mys"
